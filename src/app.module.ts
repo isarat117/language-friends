e3aea0d3d282ef341444d6fd23f39f301324b822
ismail arat <ismailarat117@gmail.com>
2024-10-08 22:24:48 +0300

    arch of project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { FollowModule } from './follow/follow.module';
+import { LanguageModule } from './language/language.module';
+import { MessagingModule } from './messaging/messaging.module';
+import { PhotoModule } from './photo/photo.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UserModule,
+    PostModule,
+    FollowModule,
+    LanguageModule,
+    MessagingModule,
+    PhotoModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
